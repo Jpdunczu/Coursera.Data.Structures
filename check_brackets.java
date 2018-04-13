2665d847224d85ec551a74547a001b4d0cfa807b
Joshua.Duncan <jd52981@gmail.com>
2018-04-13 10:50:00 -0700

    2nd commit
    - added Problem description
    - added time used from grader output

--- check_brackets.java
+++ check_brackets.java
@@ -1,3 +1,26 @@
+/*
+Problem Description
+Task. Your friend is making a text editor for programmers. He is currently working on a feature that will
+find errors in the usage of different types of brackets. Code can contain any brackets from the set
+[]{}(), where the opening brackets are [,{, and ( and the closing brackets corresponding to them
+are ],}, and ).
+For convenience, the text editor should not only inform the user that there is an error in the usage
+of brackets, but also point to the exact place in the code with the problematic bracket. First priority
+is to find the first unmatched closing bracket which either doesn’t have an opening bracket before it,
+like ] in ](), or closes the wrong opening bracket, like } in ()[}. If there are no such mistakes, then
+it should find the first unmatched opening bracket without the corresponding closing bracket after it,
+like ( in {}([]. If there are no mistakes, text editor should inform the user that the usage of brackets
+is correct.
+Apart from the brackets, code can contain big and small latin letters, digits and punctuation marks.
+More formally, all brackets in the code should be divided into pairs of matching brackets, such that in
+each pair the opening bracket goes before the closing bracket, and for any two pairs of brackets either
+one of them is nested inside another one as in (foo[bar]) or they are separate as in f(a,b)-g[c].
+The bracket [ corresponds to the bracket ], { corresponds to }, and ( corresponds to ).
+
+Time Limits. C: 1 sec, C++: 1 sec, Java: 1 sec, Python: 1 sec. C#: 1.5 sec, Haskell: 2 sec, JavaScript: 3
+sec, Ruby: 3 sec, Scala: 3 sec.
+*/
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
@@ -23,6 +46,7 @@ class Bracket {
     int position;
 }
 
+//(Max time used: 0.44/1.50, max memory used: 28868608/536870912.)
 class check_brackets {
     public static void main(String[] args) throws IOException {
         InputStreamReader input_stream = new InputStreamReader(System.in);
@@ -34,13 +58,13 @@ class check_brackets {
             char next = text.charAt(position);
 
             if (next == '(' || next == '[' || next == '{') {
-                // Process opening bracket, write your code here
+                // Process opening bracket
 		Bracket bracket = new Bracket(next,position+1);
 		opening_brackets_stack.push(bracket);
             }
 
             if (next == ')' || next == ']' || next == '}') {
-                // Process closing bracket, write your code here
+                // Process closing bracket
 		if( opening_brackets_stack.empty() ) {
 			System.out.print(position+1);
 			return;
