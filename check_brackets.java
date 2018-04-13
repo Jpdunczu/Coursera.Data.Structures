d7e94d7eb207a41bad6a133d6c91ab3bfbc8c921
Joshua.Duncan <jd52981@gmail.com>
2018-04-13 10:46:46 -0700

    1st commit

---
+++ check_brackets.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Stack;
+
+class Bracket {
+    Bracket(char type, int position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    boolean Match(char c) {
+        if (this.type == '[' && c == ']')
+            return true;
+        if (this.type == '{' && c == '}')
+            return true;
+        if (this.type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+}
+
+class check_brackets {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader input_stream = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input_stream);
+        String text = reader.readLine();
+
+        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        for (int position = 0; position < text.length(); ++position) {
+            char next = text.charAt(position);
+
+            if (next == '(' || next == '[' || next == '{') {
+                // Process opening bracket, write your code here
+		Bracket bracket = new Bracket(next,position+1);
+		opening_brackets_stack.push(bracket);
+            }
+
+            if (next == ')' || next == ']' || next == '}') {
+                // Process closing bracket, write your code here
+		if( opening_brackets_stack.empty() ) {
+			System.out.print(position+1);
+			return;
+		} else {
+			Bracket bracket = opening_brackets_stack.pop();
+			if( !bracket.Match(next) ) {
+				System.out.print(position+1);
+				return;
+			}
+		}
+            }
+        }
+	
+        // Printing answer, write your code here
+	if( opening_brackets_stack.empty() ) {
+		System.out.print("Success");
+	} else {
+		Bracket bracket = opening_brackets_stack.pop();
+		System.out.print((bracket.position));
+	}
+    }
+}
